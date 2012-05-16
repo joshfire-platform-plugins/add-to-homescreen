fe5fa824a243088c1e96b1db6b7d19f787950274
stephan83 <stephan.florquin@gmail.com>
2012-05-16 12:13:11 +0200

    Trying to figure out what is wrong.

--- build.js
+++ build.js
@@ -1,14 +1,13 @@
 module.exports = {
   startfile: function(runtime, params, cb) {
     runtime.readFile('res/add2home.css', function(err, cnt) {
-      cb(null, params['content']); return;
-      /*if (err) return cb(err);
+      if (err) return cb(err);
 
-      var html = '<style style type="text/css"></style>\n';
+      var html = '<style style type="text/css">\n';
       html    += ctn;
       html    += '\n</style></body>';
 
-      cb(null, params['content'].replace(/<\/body>/i, html));*/
+      cb(null, params['content'].replace(/<\/body>/i, html));
     });
   },
   bootstrap: function(runtime, params, cb) {
